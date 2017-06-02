ac582093249419916b506e6f1cf6272faddb0572
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-02 12:54:04 -0700

    melons take 2

--- melons.py
+++ melons.py
@@ -16,6 +16,10 @@ class AbstractMelonOrder(object):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species.lower() == "christmas melon":
+            base_price = base_price * 1.5
+       
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -62,10 +66,8 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
-        """Initialize melon order attributes."""
 
         super(InternationalMelonOrder, self).__init__(species, qty)
-
         # self.species = species
         # self.qty = qty
         self.country_code = country_code
@@ -73,13 +75,17 @@ class InternationalMelonOrder(AbstractMelonOrder):
         self.order_type = "international"
         self.tax = 0.17
 
-    # def get_total(self):
-    #     """Calculate price, including tax."""
+       
+    def get_total(self):
+        """Calculate price, including tax."""
 
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
 
-    #     return total
+        total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            total = total + 3
+
+        return total
 
     # def mark_shipped(self):
     #     """Record the fact than an order has been shipped."""
@@ -90,3 +96,19 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order from the government."""
+
+    def __init__(self, species, qty):
+
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.tax = 0.0
+        self.passed_inspection = False
+        self.order_type = "government"
+
+    def mark_inspection(self, passed):
+        if passed == 'True':
+            self.passed_inspection = True
+
